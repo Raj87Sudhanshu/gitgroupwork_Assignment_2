b7590f54d1d6de57b929c9bb16b12b3d5b9d471e
SUDHANSHU RAJ <103066186+Raj87Sudhanshu@users.noreply.github.com>
2022-10-17 23:55:36 -0700

    Update PushNotification.py

--- PushNotification.py
+++ PushNotification.py
@@ -1,1 +1,25 @@
+import requests
+from bs4 import BeautifulSoup
+from win10toast import ToastNotifier
+import re
+from time import sleep
 
+
+def ascii_art():
+    # An artistic banner for the script
+    print("""
+██╗     ██╗██╗   ██╗███████╗     ██████╗██████╗ ██╗ ██████╗██╗  ██╗███████╗████████╗    ███████╗ ██████╗ ██████╗ ██████╗ ███████╗
+██║     ██║██║   ██║██╔════╝    ██╔════╝██╔══██╗██║██╔════╝██║ ██╔╝██╔════╝╚══██╔══╝    ██╔════╝██╔════╝██╔═══██╗██╔══██╗██╔════╝
+██║     ██║██║   ██║█████╗      ██║     ██████╔╝██║██║     █████╔╝ █████╗     ██║       ███████╗██║     ██║   ██║██████╔╝█████╗  
+██║     ██║╚██╗ ██╔╝██╔══╝      ██║     ██╔══██╗██║██║     ██╔═██╗ ██╔══╝     ██║       ╚════██║██║     ██║   ██║██╔══██╗██╔══╝  
+███████╗██║ ╚████╔╝ ███████╗    ╚██████╗██║  ██║██║╚██████╗██║  ██╗███████╗   ██║       ███████║╚██████╗╚██████╔╝██║  ██║███████╗
+╚══════╝╚═╝  ╚═══╝  ╚══════╝     ╚═════╝╚═╝  ╚═╝╚═╝ ╚═════╝╚═╝  ╚═╝╚══════╝   ╚═╝       ╚══════╝ ╚═════╝ ╚═════╝ ╚═╝  ╚═╝╚══════╝""")
+
+def get_current_matches():
+    # Function to fetch the live matches currently in play
+    page = requests.get('http://static.cricinfo.com/rss/livescores.xml') # HTTP Get request to cricinfo rss feed
+    soup = BeautifulSoup(page.text,'lxml') # parsing and building xml tree out of the returned xml file
+    matches = soup.find_all('description') # description tags contain the score
+    live_matches = [s.get_text() for s in matches if '*' in s.get_text()] # returns only the live matches and ignores the completed ones
+    return live_matches
+  
